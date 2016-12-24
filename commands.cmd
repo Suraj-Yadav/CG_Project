d5030e72e3cc70aa31ac73e3b76d2020f741451c
Suraj Yadav <surajyadav20@outlook.com>
2016-12-24 12:01:21 +0530

    Addded Prim's Algorithm

--- commands.cmd
+++ commands.cmd
@@ -3,3 +3,4 @@ cd /D G:\work\CG_Project\build
 cmake -G "Visual Studio 14 2015 Win64"
 MSBuild /p:Configuration=Release 
 RMDIR /S /Q .
+forfiles /P ..\input_3D /M *.xyz /c "cmd /C ..\build\Release\test1.exe @file ..\build\output.txt" > log.txt 2>&1
